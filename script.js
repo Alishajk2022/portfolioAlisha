5b0a184876e559febfb09e92699328730d0f37b1
alisha <alisha.j@qburst.com>
2022-08-12 10:48:10 +0530

    Created message for feedback section. added few features.

--- script.js
+++ script.js
@@ -9,10 +9,15 @@ function closeNav() {
 }
 function submitMessage() {
     var Message = document.getElementById("msg").value;
+    document.getElementById("name").value = "";
+    document.getElementById("mailid").value = "";
+    document.getElementById("msg").value = "";
+    // document.getElementById("message").style.width = "0";
     alert("Message Sent")
+
 
 }
 function connectMe() {
-    document.getElementById("contactMe").style.width = '500px';
+    document.getElementById("message").style.width = '250px';
 
 }
