7c6927b82b7c0b31a3dae8053a9d775a8b41e975
alisha <alisha.j@qburst.com>
2022-08-16 16:59:24 +0530

    Changes in styling

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 function openNav() {
-    document.getElementById("navigation").style.width = "250px";
-    // document.getElementById("container").style.marginRight = "250px";
+    document.getElementById("navBar").style.width = "250px";
 }
 
 function closeNav() {
-    document.getElementById("navigation").style.width = "0";
-    // document.getElementById("container").style.marginRight = "0";
+    document.getElementById("navBar").style.width = "0";
+}
+function closeMsg() {
+    document.getElementById("message").style.width = "0";
 }
 function submitMessage() {
     var Message = document.getElementById("msg").value;
     document.getElementById("name").value = "";
     document.getElementById("mailid").value = "";
     document.getElementById("msg").value = "";
-    // document.getElementById("message").style.width = "0";
     alert("Message Sent")
-
-
 }
 function connectMe() {
     document.getElementById("message").style.width = '250px';
