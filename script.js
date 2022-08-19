4afe5a0f74c3ae1df8a728feb6d1102f0d959ef5
alisha <alisha.j@qburst.com>
2022-08-19 13:33:56 +0530

    cahnges in form validation and background

--- script.js
+++ script.js
@@ -9,12 +9,29 @@ function closeMsg() {
     document.getElementById("message").style.width = "0";
 }
 function submitMessage() {
+    var Name = document.getElementById("name").value;
+    var MailId = document.getElementById("mailid").value;
     var Message = document.getElementById("msg").value;
+
+    if (Name == "" || Name == null) {
+        alert("Name must be filled out");
+        return false;
+    }
+    if (MailId == "" || MailId == null) {
+        alert("Mail id is to be filled");
+        return false;
+    }
+    if (Message == "" || Message == null) {
+        alert("Please add message to continue");
+        return false;
+    }
+
     document.getElementById("name").value = "";
     document.getElementById("mailid").value = "";
     document.getElementById("msg").value = "";
     alert("Message Sent")
 }
+
 function connectMe() {
     document.getElementById("message").style.width = '250px';
 
